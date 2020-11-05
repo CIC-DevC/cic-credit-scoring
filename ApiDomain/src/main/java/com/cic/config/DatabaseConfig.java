f7a1fd4f3c3b235b2af84a64fb07583214bab322
Trung Duyen <trungduyen0220@gmail.com>
2020-11-05 20:00:21 +0700

    heroku config

---
+++ ApiDomain/src/main/java/com/cic/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+/**
+ * @author ntmduyen
+ * @datetime Sep 16, 2020 - 11:51:49 PM
+ */
+package com.cic.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
